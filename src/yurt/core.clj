025623e93ee8d437691daefc8e5116d562b0d65a
anatoly <tolitius@gmail.com>
2016-01-29 16:52:46 -0500

    multiple systems? ok.

--- src/yurt/core.clj
+++ src/yurt/core.clj
@@ -1,15 +1,15 @@
 (ns yurt.core
   (:require [mount.core :as mount]))
 
-(defprotocol Lifecycle
+(defprotocol Hut
   (build [this] "builds a yurt")
   (destroy [this] "destroys a yurt")
-  ;; (build-with [this components])
+  (build-with [this substitutes] "builds a yurt with some components substituted")
   ;; (build-without [this components])
   ;; (destroy-except [this components])
   )
 
-(defrecord Yurt [components])
+(defrecord Yurt [components blueprint])
 
 (defn- select-fun [states f]
   (into []
@@ -18,7 +18,7 @@
                   (when-let [fun (f state)]
                     [name fun])))))
 
-(defn- bring-down [components funs]
+(defn- bulldoze [components funs]
   (into {}
         (doseq [[name fun] funs]
           ((fun) (components name))
@@ -27,45 +27,59 @@
 (defn- unvar-state [s]
   (->> s (drop 2) (apply str)))  ;; magic 2 is removing "#'" in state name
 
+(defn- var-state [s]
+  (str "#'" s))
+
 (defn unvar-names [states]
   (into {} (for [[k v] states]
              [(unvar-state k) v])))
 
-(defn- not-started [states]
-  (into {}
-    (for [[state {:keys [order]}] states]
-      [state {:status :not-started 
-              :order order}])))
-
 (defn- detach [sys]
-  (doseq [[state {:keys [var status] :as v}] sys]
-    (alter-var-root var (constantly :not-started))
-    (#'mount.core/update-meta! [state :status] #{:stopped})
-    (#'mount.core/update-meta! [state :var] :not-started)))
+  (doseq [[state status] sys]
+    (#'mount.core/down state status (atom []))))
 
 (defn- attach [sys]
   (into {}
         (for [[k {:keys [var]}] sys]
           [(unvar-state k) @var])))
 
+(defn- var-subs [m]
+  (into {}
+        (for [[k v] m]
+          [(var-state k) v])))
+
 ;; TODO: this will take with / without / individual states
-(defn- spawn [sys]
-  (mount/start)
-  (let [spawned (attach sys)]
-    (detach sys)
+(defn- spawn [sys & {:keys [swap]}]
+  (if-not swap
+    (mount/start)
+    (mount/start-with (var-subs swap)))
+  (let [spawned (attach @sys)]
+    (detach @sys)
     spawned))
 
+(defn- not-started [states]
+  (into {}
+    (for [[state {:keys [order]}] states]
+      [state {:status :not-started}])))
+
+(defn- new-blueprint [states]
+  (into {}
+    (for [[state {:keys [order]}] states]
+      [state {:order order}])))
+
 (defn blueprint []
-  (let [meta-state @@#'mount.core/meta-state
+  (let [meta-state @#'mount.core/meta-state
         states (-> (sort-by (comp :order val) < 
-                            meta-state)
+                            @meta-state)
                     unvar-names)
-        down (reverse (select-fun states :stop))]
+        bp (new-blueprint states)
+        stop-fns (reverse (select-fun states :stop))]
     (extend-type Yurt
-      Lifecycle
-      (build [_] (->Yurt (spawn meta-state)))
-      (destroy [it] (bring-yurt (:components it) down)))
-    (->Yurt (not-started states))))
+      Hut
+      (build [_] (->Yurt (spawn meta-state) bp))
+      (build-with [_ substitutes] (->Yurt (spawn meta-state :swap substitutes) bp))
+      (destroy [it] (bulldoze (:components it) stop-fns)))
+    (->Yurt (not-started states) bp)))
 
 (comment
 
