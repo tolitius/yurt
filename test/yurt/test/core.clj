cdb705c5b656a538b28ceb7d051e872f415ec212
dhruvbhatia <dhruv@dhruvbhatia.com>
2016-11-19 14:43:53 +1100

    added yurt/build-only-with which can combine :only and :swap - see #3

--- test/yurt/test/core.clj
+++ test/yurt/test/core.clj
@@ -24,7 +24,7 @@
       (is (not (.isStarted a-www)))
       (is (not (.isStarted b-www)))))
 
-  (testing "yurt can be build with a specified scope of components"
+  (testing "yurt can be built with a specified scope of components"
     (let [blue (yurt/blueprint)
           yurt (yurt/build-only blue #{"neo.conf/config" "neo.app/nrepl"})
           www ((-> yurt :components) "neo.www/neo-app")
@@ -38,4 +38,36 @@
       (is (not (.isClosed (:server-socket repl))))
       
       (is (= (yurt/destroy yurt) {:stopped #{"neo.app/nrepl"}}))
-      (is (.isClosed (:server-socket repl))))))
+      (is (.isClosed (:server-socket repl)))))
+
+  (testing "yurt can be built with a specified scope of components and substitutions"
+           (let [blue (yurt/blueprint)
+                 a-yurt (yurt/build blue)
+                 b-config (edn/read-string (slurp "dev/resources/test-config.edn"))
+                 b-yurt (yurt/build-only-with blue #{"neo.conf/config" "neo.app/nrepl"} {"neo.conf/config" b-config})
+                 a-conf ((-> a-yurt :components) "neo.conf/config")
+                 b-conf ((-> b-yurt :components) "neo.conf/config")
+                 b-db ((-> b-yurt :components) "neo.db/db")
+                 a-repl ((-> a-yurt :components) "neo.app/nrepl")
+                 b-repl ((-> b-yurt :components) "neo.app/nrepl")
+                 a-www ((-> a-yurt :components) "neo.www/neo-app")
+                 b-www ((-> b-yurt :components) "neo.www/neo-app")]
+
+                (is (= b-conf b-config))
+                (is (not= a-conf b-config))
+
+                (is (.isStarted a-www))
+                
+                (is (not b-www))
+                (is (not b-db))
+                (is b-conf)
+                (is (not (.isClosed (:server-socket b-repl))))
+
+                (is (= (yurt/destroy b-yurt) {:stopped #{"neo.app/nrepl"}}))
+                (is (.isStarted a-www))
+                (is (not b-www))
+                (is (not (.isClosed (:server-socket a-repl))))
+                (is (.isClosed (:server-socket b-repl)))
+
+                (yurt/destroy a-yurt)
+                (is (not (.isStarted a-www))))))
